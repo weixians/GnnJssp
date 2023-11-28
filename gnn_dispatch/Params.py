36b2eb6e61ea148951bca65b1ac29c18bbc849f7
weixian <weixian_shi@outlook.com>
2023-11-28 09:59:14 +0800

    train more

--- gnn_dispatch/Params.py
+++ gnn_dispatch/Params.py
@@ -41,7 +41,7 @@ parser.add_argument("--num_mlp_layers_critic", type=int, default=2, help="No. of
 parser.add_argument("--hidden_dim_critic", type=int, default=32, help="hidden dim of MLP in critic")
 # args for PPO
 parser.add_argument("--num_envs", type=int, default=4, help="No. of envs for training")
-parser.add_argument("--max_updates", type=int, default=10000, help="No. of episodes of each env for training")
+parser.add_argument("--max_updates", type=int, default=100000, help="No. of episodes of each env for training")
 parser.add_argument("--lr", type=float, default=2e-5, help="lr")
 parser.add_argument("--decayflag", type=bool, default=False, help="lr decayflag")
 parser.add_argument("--decay_step_size", type=int, default=2000, help="decay_step_size")
